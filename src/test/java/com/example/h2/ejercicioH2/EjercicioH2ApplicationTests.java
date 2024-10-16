2bd34e1757fd944f1c9fe92e7e1c89e6a1d675c0
unknown <jmatias-2019152@kinal.edu.gt>
2024-10-16 09:55:17 -0600

    ejercicioH2

---
+++ src/test/java/com/example/h2/ejercicioH2/EjercicioH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.h2.ejercicioH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
